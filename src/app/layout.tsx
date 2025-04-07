035fd374e408cc0fd10385b86b00ab43dd29e7a0
daniel saltarin castillo <dgsaltarin@gmail.com>
2025-04-06 23:31:09 -0500

    updated favicon, title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "`Daniel Saltarin",
+  title: "Daniel Saltarin",
   description: "Personal website of Daniel Saltarin",
 };
 
