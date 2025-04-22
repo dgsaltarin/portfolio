fb22b9517b13dedbcb315c8db3a06a9ffcd6f6c7
daniel saltarin castillo <dgsaltarin@gmail.com>
2025-04-21 19:49:10 -0500

    moved google tag to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +16,32 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const googleTagId = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID;
+
   return (
     <html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        {googleTagId && (
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
+          />
+        )}
+        {googleTagId && (
+          <Script
+            id="google-analytics"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${googleTagId}');
+              `,
+            }}
+          />
+        )}
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
